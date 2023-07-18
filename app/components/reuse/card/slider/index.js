bcb31dc50dd04329bedeaa0aaece0eba72066ffe
techshomez <danieltopeg@gmail.com>
2023-07-18 17:09:16 +0100

    slider component

--- app/components/reuse/card/slider/index.js
+++ app/components/reuse/card/slider/index.js
@@ -22,33 +22,7 @@ const CardSlider = () => {
               alt=""
               className="img"
             />
-            <div className="list">
-              <span>200 maggie-lane, New York, NY, 90000</span>
-              <small>
-                <i>
-                  <FaBed />
-                </i>
-                3
-              </small>
-              <small>
-                <i>
-                  <FaBath />
-                </i>
-                1
-              </small>
-            </div>
-            <h2>Ocean breeze villa</h2>
-            <h4>$950,000.00</h4>
-          </div>
-          <div className="slider__item">
-            <Image
-              src="/images/card_image/3.jpg"
-              width={500}
-              height={100}
-              alt=""
-              className="img"
-            />
-            <div className="list">
+            <div className="slider__list">
               <span>200 maggie-lane, New York, NY, 90000</span>
               <span>
                 <i>
@@ -74,7 +48,7 @@ const CardSlider = () => {
               alt=""
               className="img"
             />
-            <div className="list">
+            <div className="slider__list">
               <span>200 maggie-lane, New York, NY, 90000</span>
               <span>
                 <i>
@@ -100,7 +74,33 @@ const CardSlider = () => {
               alt=""
               className="img"
             />
-            <div className="list">
+            <div className="slider__list">
+              <span>200 maggie-lane, New York, NY, 90000</span>
+              <span>
+                <i>
+                  <FaBed />
+                </i>
+                3
+              </span>
+              <span>
+                <i>
+                  <FaBath />
+                </i>
+                1
+              </span>
+            </div>
+            <h2>Ocean breeze villa</h2>
+            <h4>$950,000.00</h4>
+          </div>
+          <div className="slider__item">
+            <Image
+              src="/images/card_image/3.jpg"
+              width={500}
+              height={100}
+              alt=""
+              className="img"
+            />
+            <div className="slider__list">
               <span>200 maggie-lane, New York, NY, 90000</span>
               <span>
                 <i>
